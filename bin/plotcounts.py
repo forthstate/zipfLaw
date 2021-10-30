2c96181890a4d97ab89f7b94ba4dcace0eff848a
forthstate <chindan.bh@hotmail.com>
2021-10-30 15:53:36 +0000

    Fix syntax error

--- bin/plotcounts.py
+++ bin/plotcounts.py
@@ -14,7 +14,7 @@ def main(args):
                                             method='max')
     df['inverse_rank'] = 1 / df['rank']
     scatplot = df.plot.scatter(x='word_frequency',
-                               y='rank', loglog=True
+                               y='rank', loglog=True,
                                xlim=args.xlim,
                                figsize=[12, 6],
                                grid=True)
