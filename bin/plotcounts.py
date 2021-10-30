04c430eadf7bbc17b739c691d66267bc0e691f20
forthstate <chindan.bh@hotmail.com>
2021-10-31 02:10:02 +0800

    Update plot function for word-counts

--- bin/plotcounts.py
+++ bin/plotcounts.py
@@ -12,7 +12,7 @@ def main(args):
                      names=('word','word_frequency'))
     df['rank'] = df['word_frequency'].rank(ascending=False,
                                             method='max')
-    df['inverse_rank'] = 1 / df['rank']
+
     scatplot = df.plot.scatter(x='word_frequency',
                                y='rank', loglog=True,
                                xlim=args.xlim,
